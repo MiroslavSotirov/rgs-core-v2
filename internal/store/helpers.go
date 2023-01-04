ff0ee8bda065cf2be2c68bc635ef3a5e18a11a87
dLazarovEl <d.lazarov@elysiumstudios.se>
2023-01-04 11:38:42 +0200

    initial commit

--- internal/store/helpers.go
+++ internal/store/helpers.go
@@ -197,3 +197,4 @@ func (r restTransactionRequest) String() string {
 		r.ReqId, r.Token, r.Game, r.Platform, r.Mode, r.Session, r.Currency, r.Amount, r.BonusAmount, r.JpAmount, r.Category,
 		r.CampaignRef, r.CloseRound, r.Round, r.TxRef, r.Description, r.InternalStatus, r.Ttl, r.TtlStamp)
 }
+ 
