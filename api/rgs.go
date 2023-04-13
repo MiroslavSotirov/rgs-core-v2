a4ff9e0ddde30caea631e2c27e40f710bbcc3693
Henrik Meijer <henrik@elysiumstudios.se>
2023-04-13 10:36:11 +0200

    ADD mutex lock before releasing token

--- api/rgs.go
+++ api/rgs.go
@@ -66,6 +66,8 @@ func HoldToken(token string) {
 }
 
 func ReleaseToken(token string) {
+	EntryLock.Lock()
+	defer EntryLock.Unlock()
 	delete(Entries, token)
 }
 
@@ -164,7 +166,7 @@ func Routes() *chi.Mux {
 			gamestateResponse = renderGamestate(r, previousGamestate, balanceResponse, engineConfig, player)
 
 			engineDefs := engineConfig.EngineDefs
-			stakeValues, defaultBet, _, _, err := parameterSelector.GetGameplayParameters(previousGamestate.BetPerLine, player.BetLimitSettingCode, gameSlug, player.CompanyId)
+			stakeValues, defaultBet, _, _, err := parameterSelector.GetGameplayParameters(previousGamestate.BetPerLine, player.BetLimitSettingCode, gameSlug, player.BetSettingId)
 			if err != nil {
 				render.Render(w, r, ErrRender(err))
 				return
